1c913cfbe8ce55935e9177c8152216e4300e4e4a
jlhrzzz <1246297148@qq.com>
2023-11-20 21:38:20 +0800

    deploy

---
+++ precache-manifest.3dc552dadd1672f3d4f3cf674347c06c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "632eeab3d466bdf5533c960b92cad68d",
+    "url": "/moni-react/index.html"
+  },
+  {
+    "revision": "1997c4ecd65590e0bbe5",
+    "url": "/moni-react/static/css/main.52e110b3.chunk.css"
+  },
+  {
+    "revision": "7e700d43c605ac332c7d",
+    "url": "/moni-react/static/js/2.d68925af.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/moni-react/static/js/2.d68925af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1997c4ecd65590e0bbe5",
+    "url": "/moni-react/static/js/main.cc01f582.chunk.js"
+  },
+  {
+    "revision": "b97960a1b47ac39bef3b",
+    "url": "/moni-react/static/js/runtime-main.8142aebf.js"
+  }
+]);
